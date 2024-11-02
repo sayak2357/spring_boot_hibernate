3fde6652ac321db0e35a0d93b3aeb8a4156c884f
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2024-11-02 21:34:50 +0530

    injection till qualifiers

---
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    // Define constructor for dependency injection
+
+//    @Autowired
+//    public DemoController(Coach theCoach){
+//        this.myCoach = theCoach;
+//    }
+
+    @Autowired
+    public void setCoach(@Qualifier("trackCoach") Coach theCoach){
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWOrkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
